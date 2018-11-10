2fa94074816face121e0c76c05b8337b28e9ad40
Vove <1132412166@qq.com>
2018-11-10 19:58:49 +0800

    # 2018-11-10
    - 添加 示例 - 等待View消失
    - 修改 全局 - 拨打电话 支持指定卡号

--- 全局/拨打电话.lua
+++ 全局/拨打电话.lua
@@ -1,6 +1,6 @@
 --[[
     正则：
-     呼叫%
+     %呼叫%
      给%打电话
      打电话给%
 
@@ -12,7 +12,7 @@ settings = {
 config = registerSettings("system_call", settings, 1)
 
 if runtime.DEBUG then
-    -- args = {"移动"}
+-- args = {"移动"}
 -- args = {"1234"}
 end
 arg = args[1]
@@ -39,20 +39,29 @@ if (not phone) then --查找失败
     system.saveMarkedContact(arg, arg, phone)
     print(phone)
 end
-local ss = {}
-ss["卡1"] = 0
-ss["卡2"] = 1
-id = nil -- 卡号
-c_id = config.getString("card_id") --设置的值
-if (c_id == "默认") then
-    id = nil
-elseif c_id == "询问" and system.simCount >= 2 then -- sim卡大于1 手动选择卡号
-    id = singleChoiceDialog("选择卡", {"卡1", "卡2"}) -- [取消]会 抛出异常 终止
-    if (not id) then --未选择
-        return
+-- 选卡
+id = nil
+if (string.find(runtime.command, "卡一")) then
+    id = 0
+elseif string.find(runtime.command, "卡二") then
+    id = 1
+end
+if (not id) then
+    local ss = {}
+    ss["卡1"] = 0
+    ss["卡2"] = 1
+    id = nil -- 卡号
+    c_id = config.getString("card_id") --设置的值
+    if (c_id == "默认") then
+        id = nil
+    elseif c_id == "询问" and system.simCount >= 2 then -- sim卡大于1 手动选择卡号
+        id = singleChoiceDialog("选择卡", {"卡1", "卡2"}) -- [取消]会 抛出异常 终止
+        if (not id) then --未选择
+            return
+        end
+    else --其他 默认
+        id = ss[c_id]
     end
-else --其他 默认
-    id = ss[c_id]
 end
 print(phone, id)
 system.call(phone, id)
