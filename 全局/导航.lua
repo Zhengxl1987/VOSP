e5aee1b008fbe82a0656ca07e0ccd3fb94a458db
Vove <1132412166@qq.com>
2019-04-10 19:55:39 +0800

    增加我的位置

--- 全局/导航.lua
+++ 全局/导航.lua
@@ -3,13 +3,17 @@
     导航到?(附近的)?@{place} 
      带我去(附近的)?@{place}
 
+     我在哪
+     我的位置
+
     目前仅支持调用百度地图和高德地图进行导航
     支持Bmap(需手动选择地点)
 ]]
 if runtime.DEBUG then
     -- args = {"北京"}
     -- args = {"浙江杭州"}
-    runtime.command = "导航到附近的美食"
+    -- runtime.command = "导航到附近的美食"
+    runtime.command = "我在哪呀"
     argMap["place"] = "附近的美食"
 end
 site = argMap["place"]
@@ -28,6 +32,28 @@ function searchNearbyBaidu()
     intent.data = Uri.parse("baidumap://map/place/nearby?query=" .. site .. "&src=andr.baidu.openAPIdemo")
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT) -- 即使在APP内,也可跳转
     app.startActivity(intent)
+end
+
+function myPlace_Baidu()
+    requireAccessibility()
+    print(runtime.currentActivity)
+    if (not (runtime.currentActivity == "com.baidu.baidumaps.MapsActivity")) then
+        print('1')
+        system.openAppByPkg("com.baidu.BaiduMap",true)
+    end
+    a = waitForId("vw_searchbox")
+    sleep(500)
+    a.tryClick()
+    e = ViewFinder().id("tvSearchBoxInput").waitFor(1000)
+    sleep(800)
+    e.text = "我的位置"
+    ViewFinder().equalsText("搜索").tryClick()
+    sleep(500)
+    ViewFinder().containsText("正在获取你的位置").waitHide()
+    sleep(500)
+    address = waitForId("tv_address", 3000).text
+    print(address)
+    speak(address)
 end
 
 --使用高德地图导航
@@ -58,14 +84,26 @@ function navByBmap()
     app.startActivity(i)
 end
 
+c = runtime.command
+
+--我的位置
+if (c == "我的位置" or matchValues(c, "我在哪%")) then
+    if system.getAppInfo("com.baidu.BaiduMap") then --安装了百度地图
+        myPlace_Baidu()
+    else
+        speak("请安装百度地图")
+    end
+    return
+end
+
 if system.getAppInfo("com.baidu.BaiduMap") then --安装了百度地图
-    if (matchValues(runtime.command, "%附近的%")) then
+    if (matchValues(c, "%附近的%")) then
         searchNearbyBaidu()
     else
         navWithBaidu()
     end
 elseif system.getAppInfo("com.autonavi.minimap") then --安装了高德地图
-    if (matchValues(runtime.command, "%附近的%")) then
+    if (matchValues(c, "%附近的%")) then
         searchNearbyAmap()
     else
         navWithAmap()
