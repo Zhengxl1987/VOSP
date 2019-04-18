a812a8241f3ce57bbacf96ccc721c1869c5a23e8
Vove <1132412166@qq.com>
2019-04-18 21:46:08 +0800

    去除SpeechAction

--- 标记功能/语音唤醒.lua
+++ 标记功能/语音唤醒.lua
@@ -1,12 +1,9 @@
-import "cn.vove7.common.appbus.AppBus"
-import "cn.vove7.common.appbus.SpeechAction"
 type = runtime.commandType
 if (runtime.DEBUG) then
-    type = 1
+    type = -1
 end
-print(type)
 if (type == 1) then
-    AppBus.INSTANCE.post(SpeechAction(SpeechAction.ActionCode.ACTION_START_WAKEUP))
+    AppBus.INSTANCE.post("start_wakeup")
 elseif type == -1 then
-    AppBus.INSTANCE.post(SpeechAction(SpeechAction.ActionCode.ACTION_STOP_WAKEUP))
+    AppBus.INSTANCE.post("stop_wakeup")
 end
