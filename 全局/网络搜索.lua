3f265f9620cc4874eb99f7d1f963899678fa1c3f
Vove <1132412166@qq.com>
2019-08-25 01:51:30 +0800

    解决网络搜索与应用内搜索冲突

--- 全局/网络搜索.lua
+++ 全局/网络搜索.lua
@@ -4,9 +4,10 @@
     搜索@{word}
 
 ]]
+import 'java.net.URLEncoder'
 if runtime.DEBUG then
     argMap["se"] = "必应"
-    argMap["word"] = "今日新鲜事"
+    argMap["word"] = "1+5"
 end
 
 settings = {
@@ -14,7 +15,7 @@ settings = {
 }
 config = registerSettings("vove_search", settings, 1)
 
-word = argMap["word"] --关键词
+word = URLEncoder.encode(argMap["word"], 'utf-8') --关键词 通过UrlEncode
 se = argMap["se"] --搜索引擎
 
 if (not word or word == "") then
@@ -27,9 +28,14 @@ engines["搜狗"] = "https://www.sogou.com/web?query="
 engines["谷歌"] = "https://www.google.com/search?q="
 engines["必应"] = "https://cn.bing.com/search?q="
 
-if se and engines[se] then
-    print("搜索引擎" .. engines[se])
-    system.openUrl(engines[se] .. word)
+if se then
+    local e = engines[se]
+    if (e) then
+        print("搜索引擎" .. e)
+        system.openUrl(engines[se] .. word)
+    else-- 不支持的搜索引擎，或者其他应用，抛出NotSupport异常，继续执行其他指令
+        notSupport()
+    end
 else
     system.openUrl(engines[config.getString("s_engine")] .. word)
 end
