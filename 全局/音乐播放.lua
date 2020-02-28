c4d8c59bf398cdea0f90a03fecb0375cc6681453
Vove <1132412166@qq.com>
2020-02-28 14:42:18 +0800

    全局: 音乐播放

---
+++ 全局/音乐播放.lua
@@ -0,0 +1,32 @@
+--[[
+    播放音乐
+    正则: 播放(音乐|歌曲)?@{song}
+]]
+
+musicApps = {
+    "com.netease.cloudmusic",
+    "com.kugou.android",
+    "com.tencent.qqmusic",
+}
+actions = {
+    "播放",
+    "酷狗播放音乐",
+    "播放音乐",
+}
+suc = false
+for i = 1, #musicApps do
+    packageName = musicApps[i]
+    cmd = actions[i]
+    if system.hasInstall(packageName) then
+        result = utils.runAppCommand(packageName, cmd, argMap)
+        if not result then
+            toast("指令 [" .. cmd .. "] 未找到")
+        else
+            suc = true
+            return
+        end
+    end
+end
+if (not suc) then
+    app.toastError("请先安装 网易云音乐/酷狗音乐/QQ音乐", 1)
+end
